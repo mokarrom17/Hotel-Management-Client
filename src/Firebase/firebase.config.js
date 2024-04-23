0b1c384dd8c11efd175b111044897591ea15acde
mokarrom17 <mokarrombasher@gmail.com>
2024-04-23 16:34:13 +0600

    firebase installation

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYaBVzTbFyQ0nPKWHO9CJjnDEiVHOM6Ns",
+  authDomain: "hotel-manage-5d574.firebaseapp.com",
+  projectId: "hotel-manage-5d574",
+  storageBucket: "hotel-manage-5d574.appspot.com",
+  messagingSenderId: "1092643816076",
+  appId: "1:1092643816076:web:c46a26bfa04fa5871171a9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
